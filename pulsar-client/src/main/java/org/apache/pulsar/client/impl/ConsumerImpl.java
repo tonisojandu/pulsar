962d97e61109c1006cfd6b351b7e2ecbf878eb6c
tonisojandu <tonis.ojandu@gmail.com>
2023-10-03 12:35:50 +0300

    Use byte schema for DLQ topic to avoid client side schema validation. (#2)

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
@@ -2154,7 +2154,7 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
                     String originMessageIdStr = getOriginMessageIdStr(message);
                     String originTopicNameStr = getOriginTopicNameStr(message);
                     TypedMessageBuilder<byte[]> typedMessageBuilderNew =
-                            producerDLQ.newMessage(Schema.AUTO_PRODUCE_BYTES(message.getReaderSchema().get()))
+                            producerDLQ.newMessage(Schema.BYTES)
                             .value(message.getData())
                             .properties(getPropertiesMap(message, originMessageIdStr, originTopicNameStr));
                     if (message.hasKey()) {
@@ -2203,7 +2203,7 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
             try {
                 if (deadLetterProducer == null) {
                     deadLetterProducer =
-                            ((ProducerBuilderImpl<byte[]>) client.newProducer(Schema.AUTO_PRODUCE_BYTES(schema)))
+                            ((ProducerBuilderImpl<byte[]>) client.newProducer(Schema.BYTES))
                                     .initialSubscriptionName(this.deadLetterPolicy.getInitialSubscriptionName())
                                     .topic(this.deadLetterPolicy.getDeadLetterTopic())
                                     .blockIfQueueFull(false)
